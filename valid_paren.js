48d6f7c5691be67748208cb6628148a587f2f036
Jon Chen <jonathan.chen.6799@gmail.com>
2021-08-26 17:55:35 -0700

    isvalid

---
+++ valid_paren.js
@@ -0,0 +1,25 @@
+function isValid(s) {
+  // if (s.length === 1) {return false}
+  let split = s.split("")
+  let left = ["(", "[", "{"]
+  let right = [")", "]", "}"]
+
+  let tmp = []
+
+  for (let i = 0; i < split.length; i++) {
+    if (left.includes(split[i])) {
+      tmp.push(split[i])
+    } else {
+      if (tmp.pop() !== left[right.indexOf(split[i])]) {
+        return false
+      }
+    }
+  }
+  if (tmp.length) {
+    return false
+  } else {
+    return true
+  }
+}
+
+console.log(isValid("()"))
